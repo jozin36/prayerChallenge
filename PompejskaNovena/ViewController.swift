2d128c063683a1f0c1bb1158eb6fc5c1e61487d0
Jozef <jozef.pazurik@gmail.com>
2025-07-17 10:39:00 +0200

    Initial Commit

---
+++ PompejskaNovena/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PompejskaNovena
+//
+//  Created by Jozef Pazúrik on 17/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
